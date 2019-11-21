1578f2279cbe189654ba2355db9c331d0482d3f8
Daniel <danielr5283@gmail.com>
2019-11-21 16:35:39 -0500

    Vistas de home, contact, services, mission y vision

--- src/components/views/Home.jsx
+++ src/components/views/Home.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import "../../style/view/home.css";
 import CardFeature from "../components/CardFeature";
+import CardComments from "../components/CardComments";
+import CardBanner from "../components/CardBanner";
 
 
 const Home = () => {
   return (
-   <div>
+    <div>
+      <CardBanner></CardBanner>
+     <CardComments></CardComments>
      <CardFeature></CardFeature>
    </div>
   );
